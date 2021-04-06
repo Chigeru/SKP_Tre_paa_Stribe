5a1ed9ef7d95abc0abb46dc90d636f8106b83f35
Frank <frankjensen156@gmail.com>
2021-04-06 14:21:38 +0200

    upload tre på stribe

---
+++ KrydsOgBolle/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace KrydsOgBolle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 300);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(515, 5);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(52, 477);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(515, 5);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.Location = new System.Drawing.Point(396, 134);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(5, 515);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Black;
+            this.pictureBox4.Location = new System.Drawing.Point(219, 134);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(5, 515);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(53, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 160);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F);
+            this.button2.Location = new System.Drawing.Point(230, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 160);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(407, 134);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 160);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(53, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 160);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(230, 311);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(160, 160);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(407, 311);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(160, 160);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(53, 488);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(160, 160);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(230, 488);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(160, 160);
+            this.button8.TabIndex = 19;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(407, 488);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(160, 160);
+            this.button9.TabIndex = 20;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(170, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 50);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Player 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(640, 664);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ KrydsOgBolle/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KrydsOgBolle
+{
+    public partial class Form1 : Form
+    {
+        int player = 0;
+
+        //Holder styr på spillet her bagved i stedet for på GUI
+        List<String> board = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            
+            Player_turn();
+            int i = 0;
+
+            //Får fat på knapperne igennem Form1 og opdaterer dem
+            foreach (var button in this.Controls.OfType<Button>())
+            {
+                button.Click += new System.EventHandler(this.Box_Chooser);
+                button.Font = new Font(button.Font.FontFamily, 72);
+                board.Add(" ");
+                button.Text = " ";
+                
+                //Bruges som en falsk form for ID, for nemmere kunne tilgåes igennem board array
+                button.Tag = i;
+                button.TabStop = false;
+                i++;
+            }
+        }
+
+        //Opdaterer spillerens valgte felt med spillerens tegn
+        private void Box_Chooser(object sender, EventArgs e)
+        {
+            Button triggeredButton = (Button)sender;
+            if (player == 1)
+            {
+                board[(Int32)triggeredButton.Tag] = "X";
+                triggeredButton.Text = board[(Int32)triggeredButton.Tag];
+            }
+            else
+            {
+                board[(Int32)triggeredButton.Tag] = "O";
+                triggeredButton.Text = board[(Int32)triggeredButton.Tag];
+            }
+            triggeredButton.Enabled = false;
+            Win_Checker();
+            Player_turn();
+        }
+
+        //Skifter spillerens tur og opdaterer label i toppen
+        private void Player_turn()
+        {
+            if (player != 1)
+            {
+                player = 1;
+                label1.Text = $"Player {player}'s turn - X";
+            }
+            else
+            {
+                player = 2;
+                label1.Text = $"Player {player}'s turn - 0";
+            }
+        }
+
+        //Tjekker om der er vundet eller om pladen er fyldt
+        private void Win_Checker()
+        {
+            //Tjekker vandret 
+            if (board[0] == board[1] && board[1] == board[2] && board[0] != " ") { Win_Annoucer(true); }
+            else if (board[3] == board[4] && board[4] == board[5] && board[3] != " ") { Win_Annoucer(true); }
+            else if (board[6] == board[7] && board[7] == board[8] && board[6] != " ") { Win_Annoucer(true); }
+
+            //Tjekker på kryds
+            else if (board[0] == board[4] && board[4] == board[8] && board[0] != " ") { Win_Annoucer(true); }
+            else if (board[2] == board[4] && board[4] == board[6] && board[2] != " ") { Win_Annoucer(true); }
+
+            //Tjekker lodret
+            else if (board[0] == board[3] && board[3] == board[6] && board[0] != " ") { Win_Annoucer(true); }
+            else if (board[1] == board[4] && board[4] == board[7] && board[1] != " ") { Win_Annoucer(true); }
+            else if (board[2] == board[5] && board[5] == board[8] && board[2] != " ") { Win_Annoucer(true); }
+
+            //Tjekker om alle felter er udfyldte
+            else if (board[0] != " " &&
+                board[1] != " " &&
+                board[2] != " " &&
+                board[3] != " " &&
+                board[4] != " " &&
+                board[5] != " " &&
+                board[6] != " " &&
+                board[7] != " " &&
+                board[8] != " ") { Win_Annoucer(false); }
+        }
+
+        private void Win_Annoucer(bool winner)
+        {
+            //Disabler alle felterne
+            foreach (Button b in this.Controls.OfType<Button>())
+                b.Enabled = false;
+
+            //Besked til dialog vinduet
+            string message = "";
+            if (winner)
+                message = $"Player {player} has won!!!\ntry again?";
+            else
+                message = "No more moves available \ntry again?";
+
+            //Dialogboxen som vises efter spillet er slut
+            DialogResult res = MessageBox.Show(message, "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (res == DialogResult.Yes)
+                Application.Restart();
+            else if (res == DialogResult.No)
+                Application.Exit();
+            else
+                Application.Exit();
+        }
+    }
+}
